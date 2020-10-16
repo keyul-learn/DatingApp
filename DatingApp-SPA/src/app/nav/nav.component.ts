a9b8f18bbfb1337abfd0f4970c1bff3da636e2c2
Keyul <keyul@yahoo.com>
2020-10-16 17:10:30 +0530

    End of section 4

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -20,4 +20,14 @@ export class NavComponent implements OnInit {
       console.log('Failed');
     });
   }
+
+  loggedIn(){
+    const token = localStorage.getItem('token');
+    return !! token;
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
 }
